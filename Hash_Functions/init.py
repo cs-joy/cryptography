1daf5736edb31cac549695e0f9a19548daedc934
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2024-07-30 22:25:15 +0600

    Update init.py

--- Hash_Functions/init.py
+++ Hash_Functions/init.py
@@ -1,4 +1,4 @@
 import hashlib, binascii
 
 SHA3_256_Hash = hashlib.sha3_256(b'hello world').digest()
-print("SHA3-256('hello world') = ", binascii.hexlify(SHA3_256_Hash))
+print("SHA3-256('hello world') = ", binascii.hexlify(SHA3_256_Hash)) # b'644bcc7e564373040999aac89e7622f3ca71fba1d972fd94a31c3bfbf24e3938'
